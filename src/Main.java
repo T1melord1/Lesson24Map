923dba81e7df870f9ea866d8a0f19413c32c24cd
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-02-20 12:35:05 +0300

    containsKey выводит true если в nameAge есть такой ключ

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,11 @@ import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
-    Map<String,Integer> nameAge = new TreeMap<>();
-    nameAge.put("Алиса", 31);
-    nameAge.put("Вася", 12);
-    nameAge.put("Петя", 44);
+        Map<String, Integer> nameAge = new TreeMap<>();
+        nameAge.put("Алиса", 31);
+        nameAge.put("Вася", 12);
+        nameAge.put("Петя", 44);
+        System.out.println(nameAge.containsKey("Петя"));
 
 
         for (Map.Entry<String, Integer> entry : nameAge.entrySet()) {
