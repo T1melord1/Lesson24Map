1307107cf1f5929d2ba1405fb7d8677bc605f59c
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-02-20 12:17:32 +0300

    nameAge.put("Алиса", 31);     nameAge.put("Вася", 12);     nameAge.put("Петя", 44);
        Каждая такая пара и называется Entry, а набор таких пар EntrySet

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,8 @@ import java.util.TreeMap;
 public class Main {
     public static void main(String[] args) {
     Map<String,Integer> nameAge = new TreeMap<>();
+    nameAge.put("Алиса", 31);
+    nameAge.put("Вася", 12);
+    nameAge.put("Петя", 44);
     }
 }
