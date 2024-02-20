bcb8724ed07c26850a6d8c23ff06fb0a27c53130
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-02-20 12:53:02 +0300

    В картах на месте ключа всегда должен быть НЕИЗМЕНЯЕМЫЙ ОБЪЕКТ, НАПРИМЕР СТРОКИ(СТРОКИ ЭТО МАССИВ). Как правильно в картах на месте ключа находятся String

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,66 @@
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Map<String, Integer> nameAge = new TreeMap<>();
-        nameAge.put("Алиса", 31);
-        nameAge.put("Вася", 12);
-        nameAge.put("Петя", 44);
-        System.out.println(nameAge.containsKey("Петя"));
+      Map<String, Set<String>> nameTodos = new HashMap<>();
+      Set<String> todosVasya = new TreeSet<>();
+
+      todosVasya.add("Купить молока");
+      todosVasya.add("Сходить в гараж");
+
+      nameTodos.put("Вася", todosVasya);
+
+      Set<String> todosPetya = new TreeSet<>();
+
+      todosPetya.add("Купить акций");
+      todosPetya.add("Купить крипту");
+
+      nameTodos.put("Петя", todosPetya);
+
+        System.out.println(nameTodos.get("Вася"));
+
+        for (String вася :  nameTodos.get("Вася")) {
+            System.out.println(вася);
+        }
+        for(Map.Entry<String, Set<String>> entry : nameTodos.entrySet()){
+            System.out.println(entry.getKey() +" " + entry.getValue());
+        }
+
+        for(Map.Entry<String, Set<String>> entry : nameTodos.entrySet()){
+            String name = entry.getKey();
+           System.out.println(name);
+           for (String todo : entry.getValue()) {
+               System.out.println("\t" + todo);
+           }
+        }
+
+        System.out.println(nameTodos.containsKey("Петя"));
 
 
-        for (Map.Entry<String, Integer> entry : nameAge.entrySet()) {
-            System.out.println(entry.getKey() + " " + entry.getValue());
+        System.out.println(nameTodos.containsValue("Купить акций"));
+
+        for(Map.Entry<String, Set<String>> entry : nameTodos.entrySet()){
+            for(String val : entry.getValue()){
+                if(val.equals("Купить акций")){
+                    System.out.println(val + " содержится в делах " + entry.getKey());
+                }else
+                    System.out.println("Нет такого дела ");
+
+            }
         }
-        for (String s : nameAge.keySet()) {
-            System.out.println(s);
+        boolean isContains = false;
+        for(Map.Entry<String, Set<String>> entry : nameTodos.entrySet()){
+            for(String val : entry.getValue()){
+                if(val.equals("Купить акций")){
+                    System.out.println(val + " содержится в делах " + entry.getKey());
+                    isContains = true;
+                }
+
+            }
         }
-        for (Integer value : nameAge.values()) {
-            System.out.println(value);
+        if(!isContains){
+            System.out.println("Такого значения нет");
         }
     }
+
 }
